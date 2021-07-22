4b9ec0c8eea69b571e605d0a7606e70af884d75b
Imdad <imdadadelabou0@gmail.com>
2021-07-22 14:29:10 +0100

    finish assigments

---
+++ assignment_ruby1/false_data.rb
@@ -0,0 +1,15 @@
+require "cgi"
+
+cgi = CGI.new
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+    # 受け取ったデータcgi['input']で取り出し、ローカル変数に代入する
+    # 目印の'input'から情報を取り出す
+    get = cgi['falseValue']
+    #Return response in HTML
+    "<html>
+      <body>
+        <p>受け取ったStringは下記になります</p>
+        <p>String：#{get}</p>
+      </body>
+    </html>"
+}
--- assignment_ruby1/true_data.rb
+++ assignment_ruby1/true_data.rb
@@ -4,7 +4,7 @@ cgi = CGI.new
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
     # 受け取ったデータcgi['input']で取り出し、ローカル変数に代入する
     # 目印の'input'から情報を取り出す
-    get = cgi['input']
+    get = cgi['Send']
     #Return response in HTML
     "<html>
       <body>
